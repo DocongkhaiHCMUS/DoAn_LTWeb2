7ef322ebb4367829224f0b589c4805902e1f6d07
1760447-NguyenThanhTri <tringuyen291999@gmail.com>
2020-06-21 23:08:22 +0700

    File py to crawl data website

---
+++ main.py
@@ -0,0 +1,152 @@
+import scrapy
+import json
+import os
+import re
+import time
+import sys
+import requests
+from scrapy.crawler import CrawlerProcess, CrawlerRunner
+from scrapy.utils.project import get_project_settings
+from scrapy.utils.log import configure_logging
+from urllib import parse as UrlParser
+from multiprocessing import Process, Queue
+from twisted.internet import reactor
+import xlsxwriter
+
+
+#WorkBook = xlsxwriter.Workbook('database.xlsx')
+
+class VnExpress(scrapy.Spider):
+    name = 'vnexpress'
+    url = 'https://vnexpress.net/'
+    Sheet =''
+    num_row = 1
+    topic = ''
+    def start_requests(self):
+        yield scrapy.Request(url=self.url, callback=self.parse)
+    def parse(self, res):
+        #print('\n\n\n\nRunning........................')
+        #print(res)
+        url_title = ''
+        titles = res.xpath('//nav[@class="main-nav"]/ul/li')
+        for item in range(4,len(titles)):
+            print(str(item - 3) +' '+ titles.xpath('a/text()')[item].get().strip())
+        
+        #print('\n\n\n\n************************END************************')
+        #articles = res.xpath('//div[@class="col-left-folder-v2"]//article[@class="item-news item-news-common"]')
+        topic = int(input('Title : ')) + 1
+        self.topic = titles[topic].xpath('a/text()').get().strip().title()
+        
+        self.Sheet = WorkBook.add_worksheet(self.topic)
+        self.Sheet.write("A1", "ID")
+        self.Sheet.write("B1", "Title")
+        self.Sheet.write("C1", "Description")
+        self.Sheet.write("D1", "Content")
+        self.Sheet.write("E1", "Name-Img")
+        self.Sheet.write("F1", "Publish Date")
+        self.Sheet.write("G1", "Link-Img")
+
+        url_title = self.url + titles[topic].xpath('a/@href').get().replace('/', '')
+        yield scrapy.Request(url=url_title, callback=self.parse_title)
+        
+        #print('\n\n\n\n************************END************************')
+    
+    
+    
+    def parse_title(self, res):
+       
+        
+        #print('\n\n\n\nRunning Parse_title........................')
+        #print(res)
+        articles = res.xpath('//article[@class="item-news item-news-common" or @class="item-news item-news-common off-thumb" or @data-offset]')
+        
+        url_article = []
+        for item in articles:
+            url = item.xpath('h3/a/@href').get()
+            if url is not None:
+                url_article.append(url)
+        for url in url_article:
+            yield scrapy.Request(url=url, callback=self.parse_article)
+                
+            
+        next_page = self.url + res.xpath('//a[@class="btn-page next-page "]/@href').get().replace('/','')
+        if next_page is not None:
+            
+            yield scrapy.Request(url=next_page,callback=self.parse_title)
+
+        
+        #print('\n\n\n\n************************END************************')
+       
+
+
+    def parse_article(self, res):
+        #print('\n\n\n\nRunning Parse_Article........................')
+        #print(res)
+          
+        
+        title = res.xpath('//h1/text()').get()
+        pub_date = res.xpath('//span[@class = "date"]/text()').get()
+        description = res.xpath('//p[@class="description"]/text()').get()
+        img_link = res.xpath('//img[@itemprop="contentUrl"]/@data-src').get() if not None else "no_link"
+        content = ''
+        content_ = res.xpath('//p[@class = "Normal"]/text()').getall()
+        for item in content_:
+            content = content + item.strip()
+        
+        dir_path = "Images/{}".format(self.topic)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        
+       
+        img_name = '{}_img_{}'.format(self.topic.replace(' ','').lower(),self.num_row) if img_link is not None  else "no_image"
+        if img_link is not None:
+            img_data = requests.get(img_link).content
+            with open('Images/{}/{}.jpg'.format(self.topic,img_name), 'wb') as handler:
+                handler.write(img_data)
+
+        # print(title ,end='\n\n')
+        # print(pub_date, end='\n\n')
+        # print(description, end='\n\n')
+        # print(img_link, end='\n\n')
+        # print(content, end='\n\n')
+
+       
+        self.Sheet.write(self.num_row, 0, self.num_row)
+        self.Sheet.write(self.num_row, 1, title)
+        self.Sheet.write(self.num_row, 2, description)
+        self.Sheet.write(self.num_row, 3, content)
+        self.Sheet.write(self.num_row, 4, img_name)
+        self.Sheet.write(self.num_row, 5, pub_date)
+        self.Sheet.write(self.num_row, 6, img_link)
+
+        print("Write Article {} : -> DONE".format(self.num_row),end='\n\n')
+        self.num_row += 1
+        
+        #print('\n\n\n\n************************END************************')
+
+
+def sleep(_, duration=5):
+    print(f'sleeping for: {duration}')
+    time.sleep(duration)  # block here
+
+
+def crawl(runner):
+    d = runner.crawl(VnExpress)
+    d.addBoth(sleep)
+    d.addBoth(lambda _: crawl(runner))
+    return d
+
+
+def Run():
+    runner = CrawlerRunner(get_project_settings())
+    crawl(runner)
+    reactor.run()
+
+WorkBook = xlsxwriter.Workbook(input('Name Database: ') + '.xlsx')
+
+if __name__ == "__main__":
+    # process = CrawlerProcess()
+    # process.crawl(VnExpress)
+    # process.start()
+    Run()
+    WorkBook.close()
