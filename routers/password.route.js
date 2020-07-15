1330a5dc492d9394a4fa3c2a592b3da37b4ccfbb
Lê Ngọc hiệp <hiepc3kt@gmail.com>
2020-07-15 21:19:24 +0700

    edit router change password

--- routers/password.route.js
+++ routers/password.route.js
@@ -3,17 +3,25 @@ const bcrypt = require('bcrypt');
 const db = require('../db/util/db.json');
 const router = express.Router();
 const nodemailer = require('nodemailer');
-router.get('/change', function (req, res) {
-    res.render('viewPass/change', {
-        layout: false
-    });
+const userModel = require('../models/user.model');
+router.get('/change',async function (req, res) {
+    if (!req.session.isAuthenticated) {     
+        res.redirect('/');
+    }
+    else {            
+        res.render('viewPass/change', {
+            layout: false
+        });
+    }
 })
-router.post('/change', function (req, res) {
-    var user = db.getData();
-    const oldpass = req.body.old_password;
-    const newpass = req.body.new_password;
-    const firmpass = req.body.firm_password;
-    const rs = bcrypt.compareSync(oldpass, user.password);
+router.post('/change',async function (req, res) {
+    let user = await userModel.singleByID(req.session.authUser.id);
+    var us = user[0];
+    console.log(us);
+    var oldpass = req.body.old_password;
+    var newpass = req.body.new_password;
+    var firmpass = req.body.firm_password;
+    var rs = bcrypt.compareSync(oldpass, us.password);
     if (rs === false) {
         return res.render('viewPass/change', {
             err: 'Old password Invalid.',
@@ -22,21 +30,21 @@ router.post('/change', function (req, res) {
     }
     if (newpass != firmpass) {
         return res.render('viewPass/change', {
-            err: 'New password incorrect.',
+            err: 'Re-password incorrect.',
             layout: false
         })
     }
-    user.password = bcrypt.hashSync(firmpass, 10);
-    db.setData(user);
-    res.redirect('/pass/change');
+    us.password = bcrypt.hashSync(firmpass, 10);
+    userModel.patch(us);
+    res.redirect('/');
 })
 router.get('/forgot', function (req, res) {
     res.render('viewPass/forgot', {
         layout: false
     });
 });
-function DestroyOTP(otp){
- otp = 0;
+function DestroyOTP(otp) {
+    otp = 0;
 }
 router.post('/forgot', function (req, res) {
     var transporter = nodemailer.createTransport({ // config mail server
@@ -52,7 +60,7 @@ router.post('/forgot', function (req, res) {
             rejectUnauthorized: false
         }
     });
-    var OTP =Math.trunc(Math.random() * (999999 - 100000) + 100000);
+    var OTP = Math.trunc(Math.random() * (999999 - 100000) + 100000);
     var content = '';
     content += `
         <div style="padding: 10px; background-color: #003375">
@@ -69,31 +77,31 @@ router.post('/forgot', function (req, res) {
         from: 'NQH-Test nodemailer',
         to: 'hiepc3kt@gmail.com',
         subject: 'Đặt lại mật khẩu',
-        text: 'Your text is here',//Thường thi mình không dùng cái này thay vào đó mình sử dụng html để dễ edit hơn
+        text: 'Your text is here',
         html: content //Nội dung html mình đã tạo trên kia :))
     }
     transporter.sendMail(mainOptions, function (err, info) {
         if (err) {
             console.log(err);
             req.flash(mess, 'Lỗi gửi mail: ' + err); //Gửi thông báo đến người dùng
-            res.redirect('/pass/forgot',err);
+            res.redirect('/pass/forgot', err);
         } else {
             console.log('Message sent: ' + info.response);
-         //   mess = 'Một email đã được gửi đến tài khoản của bạn'
+            //   mess = 'Một email đã được gửi đến tài khoản của bạn'
             req.flash(mess, 'Một email đã được gửi đến tài khoản của bạn'); //Gửi thông báo đến người dùng
             res.render('viewPass/newpass', {
                 layout: false
             });
         }
     });
-    var data ={
-        OTP : OTP,
-        id :1
+    var data = {
+        OTP: OTP,
+        id: 1
     }
-     res.render('viewPass/newpass', {
-         layout: false,data
-     });
-     setTimeout(DestroyOTP(OTP),86400000);
+    res.render('viewPass/newpass', {
+        layout: false, data
+    });
+    setTimeout(DestroyOTP(OTP), 86400000);
 })
 router.get('/new', function (req, res) {
     res.render('viewPass/newpass', {
@@ -103,7 +111,8 @@ router.get('/new', function (req, res) {
 router.post('/new', function (req, res) {
     // xử lí update password
     res.render('viewPass/newpass', {
-        layout: false
+        layout: false,
+        err: 'Re-password Invalid.'
     });
 });
 module.exports = router;
