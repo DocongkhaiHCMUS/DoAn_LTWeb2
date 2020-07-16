97a2db20a009f095348052db3af8ec4adf9bb16b
Lê Ngọc hiệp <hiepc3kt@gmail.com>
2020-07-16 12:25:56 +0700

    update forgot password

--- routers/password.route.js
+++ routers/password.route.js
@@ -4,17 +4,18 @@ const db = require('../db/util/db.json');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 const userModel = require('../models/user.model');
-router.get('/change',async function (req, res) {
-    if (!req.session.isAuthenticated) {     
+const jwt = require('jsonwebtoken');
+router.get('/change', async function (req, res) {
+    if (!req.session.isAuthenticated) {
         res.redirect('/');
     }
-    else {            
+    else {
         res.render('viewPass/change', {
             layout: false
         });
     }
 })
-router.post('/change',async function (req, res) {
+router.post('/change', async function (req, res) {
     let user = await userModel.singleByID(req.session.authUser.id);
     var us = user[0];
     console.log(us);
@@ -43,76 +44,142 @@ router.get('/forgot', function (req, res) {
         layout: false
     });
 });
-function DestroyOTP(otp) {
-    otp = 0;
-}
-router.post('/forgot', function (req, res) {
-    var transporter = nodemailer.createTransport({ // config mail server
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: 'lnhiep120699@gmail.com', //Tài khoản gmail vừa tạo
-            pass: 'hiepktvn99' //Mật khẩu tài khoản gmail vừa tạo
-        },
-        tls: {
-            // do not fail on invalid certs
-            rejectUnauthorized: false
+var generateToken = (user, secretSignature, tokenLife) => {
+    return new Promise((resolve, reject) => {
+        // Định nghĩa những thông tin của user muốn lưu vào token ở đây
+        const userData = {
+            id: user.id,
+            user_name: user.user_name,
+            email: user.email,
         }
+        // Thực hiện ký và tạo token
+        jwt.sign(
+            { data: userData },
+            secretSignature,
+            {
+                algorithm: "HS256",
+                expiresIn: tokenLife,
+            },
+            (error, token) => {
+                if (error) {
+                    return reject(error);
+                }
+                resolve(token);
+            });
     });
-    var OTP = Math.trunc(Math.random() * (999999 - 100000) + 100000);
-    var content = '';
-    content += `
+}
+var verifyToken = (token, secretKey) => {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, secretKey, (error, decoded) => {
+            if (error) {
+                return reject(error);
+            }
+            resolve(decoded);
+        });
+    });
+}
+//60000 = 60s
+const accessTokenSecret = "access-token-secret-@hiep";
+const accessTokenLife = process.env.ACCESS_TOKEN_LIFE || "1h";
+router.post('/forgot', async function (req, res) {
+    const user = await userModel.singleByUserName(req.body.username);
+    const us = user[0];
+    if (!us) {
+        res.render('viewPass/forgot.hbs', {
+            layout: false,
+            err: 'Username is not valid.'
+        });
+    }
+    else {
+        var transporter = nodemailer.createTransport({ // config mail server
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: process.env.MAILER_EMAIL_ID, //Tài khoản gmail server
+                pass: process.env.MAILER_PASSWORD //Mật khẩu tài khoản gmail server
+            },
+            tls: {
+                // do not fail on invalid certs
+                rejectUnauthorized: false
+            }
+        });
+        const userData = {
+            id: us.id,
+            user_name: us.user_name,
+            email: us.email,
+        };
+        const accessToken = await generateToken(userData, accessTokenSecret, accessTokenLife);
+        var content = '';
+        content += `
         <div style="padding: 10px; background-color: #003375">
             <div style="padding: 10px; background-color: white;">
-                <h4 style="color: #0085ff">Dear HiepGa</h4>
-                <span style="color: black">Bạn đã yêu cầu đặt lại mật khẩu cho tài khoản hiepkt</span>
-                <p> Mã OTP của bạn là : <b>${OTP}</b></p> 
-                <p style="color: red"> Mã sẽ hết hạn sau 3 giờ </p> 
-                <p> Vui lòng không phản hồi lại thư này </p> 
+                <h4 style="color: #0085ff">Dear ${us.display_name}</h4>
+                <span style="color: black">You have selected to reset password for ${us.user_name}</span>
+                <p> Your OTP : <b>${accessToken}</b></p> 
+                <p style="color: red"> This code will expire ${accessTokenLife} after this email was send</p> 
+                <p> If you did not make this request , you can ignore this email </p> 
             </div>
         </div>
     `;
-    var mainOptions = { // thiết lập đối tượng, nội dung gửi mail
-        from: 'NQH-Test nodemailer',
-        to: 'hiepc3kt@gmail.com',
-        subject: 'Đặt lại mật khẩu',
-        text: 'Your text is here',
-        html: content //Nội dung html mình đã tạo trên kia :))
+        var mainOptions = { // thiết lập đối tượng, nội dung gửi mail
+            from: 'NQH-Test nodemailer',
+            to: us.email,
+            subject: 'Reset password',
+            text: 'Your text is here',
+            html: content //Nội dung html đã tạo trên kia 
+        }
+        transporter.sendMail(mainOptions, function (err, info) {
+            if (err) {
+                res.render('viewPass/forgot.hbs', {
+                    layout: false,
+                    err
+                });
+            } else {
+                us.accessToken = accessToken;
+                userModel.patch(us);
+                console.log('Message sent: ' + info.response);
+                res.render('viewPass/newpass', {
+                    layout: false
+                });
+            }
+        });
     }
-    transporter.sendMail(mainOptions, function (err, info) {
-        if (err) {
-            console.log(err);
-            req.flash(mess, 'Lỗi gửi mail: ' + err); //Gửi thông báo đến người dùng
-            res.redirect('/pass/forgot', err);
-        } else {
-            console.log('Message sent: ' + info.response);
-            //   mess = 'Một email đã được gửi đến tài khoản của bạn'
-            req.flash(mess, 'Một email đã được gửi đến tài khoản của bạn'); //Gửi thông báo đến người dùng
-            res.render('viewPass/newpass', {
-                layout: false
+
+})
+router.post('/new', async function (req, res) {
+    // xử lí update password
+    const newPass = req.body.password;
+    const rePass = req.body.firmpassword;
+    const token = req.body.otp;
+    if (newPass != rePass) {
+        res.render('viewPass/newpass.hbs', {
+            layout: false,
+            err: 'Password does not match.'
+        });
+    }
+    else {
+        try {
+            const decoded = await verifyToken(token, accessTokenSecret);
+            const user = await userModel.singleByID(decoded.data.id);
+            const us = user[0];
+            if (token === us.accessToken) {
+                us.password = bcrypt.hashSync(rePass, 10);
+                userModel.patch(us);
+                res.redirect('/login');
+            }
+            else{
+                res.render('viewPass/newpass.hbs', {
+                    layout: false,
+                    err: 'OTP invalid'
+                });
+            }
+        } catch (error) {
+            res.render('viewPass/newpass.hbs', {
+                layout: false,
+                err: 'OTP invalid or expired.'
             });
         }
-    });
-    var data = {
-        OTP: OTP,
-        id: 1
     }
-    res.render('viewPass/newpass', {
-        layout: false, data
-    });
-    setTimeout(DestroyOTP(OTP), 86400000);
-})
-router.get('/new', function (req, res) {
-    res.render('viewPass/newpass', {
-        layout: false
-    });
-});
-router.post('/new', function (req, res) {
-    // xử lí update password
-    res.render('viewPass/newpass', {
-        layout: false,
-        err: 'Re-password Invalid.'
-    });
 });
 module.exports = router;
