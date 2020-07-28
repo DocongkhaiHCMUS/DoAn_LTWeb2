1bd5c3d880866d9e650f8f8b2605f72ce290b86f
Lê Ngọc hiệp <hiepc3kt@gmail.com>
2020-07-28 13:21:03 +0700

    update restore catvl1

--- routers/admin.route.js
+++ routers/admin.route.js
@@ -64,22 +64,38 @@ router.post('/category/update', async function (req, res) {
     await cateModel.patch1(req.body);
     res.redirect('/admin/category');
 });
-
+router.post('/category/restore', async function (req, res) {
+    var entity = {
+        id: req.body.id,
+        name: req.body.name,
+        delete: 0
+    }
+    await cateModel.patch1(entity);
+    res.redirect('/admin/category');
+});
 router.get('/category/detail/:catID', async function (req, res) {
     const categoryLv2 = await cateModel.single2(req.params.catID);
-    let i = 0 ;
+    let i = 0;
     while (i < categoryLv2.length) {
         categoryLv2[i].create_date = moment(categoryLv2[i].create_date, 'YYYY-MM-DD HH:mm:ss').format('DD/MM/YYYY HH:mm:ss');
         categoryLv2[i].modifile_date = moment(categoryLv2[i].modifile_date, 'YYYY-MM-DD HH:mm:ss').format('DD/MM/YYYY HH:mm:ss');
         i++;
     }
-    
+
     res.render('viewAdmin/viewCategory/detail.hbs', {
         layout: false,
         category: categoryLv2,
         empty: categoryLv2.length === 0
     });
 });
+router.get('/categorylv2/edit/:id',async function (req, res) {
+    const id = +req.params.catID || -1;
+    const rows = await cateModel.single1(id);
+    if (rows.length === 0)
+        return res.send('Invalid parameter.');
 
+    const category = rows[0];
+    res.render('viewAdmin/viewCategory/edit.hbs', { layout: false, category });
+});
 //Xu li POST
 module.exports = router;
