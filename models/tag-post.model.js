175006e0b15bb3de8d82593a01968db6b0c36db4
Lê Ngọc hiệp <hiepc3kt@gmail.com>
2020-07-30 12:33:03 +0700

    edit tag post model

--- models/tag-post.model.js
+++ models/tag-post.model.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 
 const TBL_TP = 'tag_post';
 const TBL_Tag = 'tag';
-
+const TBL_Post = 'post';
 module.exports = {
     load: () => db.load(`select tp.id, tp.tag, tag.name, tp.post from ${TBL_TP} tp join ${TBL_Tag} tag on tp.tag = tag.id and tp.delete = 0`),
 
@@ -11,13 +11,15 @@ module.exports = {
         return db.load(`select tp.id, tp.tag, tag.name from ${TBL_TP} tp join ${TBL_Tag} tag on tp.tag = tag.id and tp.post = '${postID}' 
                         and tp.delete = 0`)
     },
-
+    singleByTag: (tagID) => {
+        return db.load(`select tag.id as tagid,p.id as postid ,p.title,tp.delete,tp.id as tpid from ${TBL_TP} tp join ${TBL_Tag} tag on tp.tag = tag.id join ${TBL_Post} p on p.id = tp.post where tag.id = '${tagID}'`)
+    },
     add: (entity) => {
 
         entity['create_date'] = moment().format('YYYY/MM/DD HH:mm:ss');
         entity['modifile_date'] = moment().format('YYYY/MM/DD HH:mm:ss');
 
-        return db.add(TBL_User, entity)
+        return db.add(TBL_TP, entity)
     },
 
     delete: (ID) => {
@@ -32,7 +34,21 @@ module.exports = {
         entity['modifile_date'] = moment().format('YYYY/MM/DD HH:mm:ss');
 
 
-        return db.patch(TBL_User, entity, condition)
+        return db.patch(TBL_TP, entity, condition)
+    },
+    restore: (ID) => {
+        let condition = {
+            id: ID
+        }
+
+        let entity = {
+            delete: 0
+        }
+
+        entity['modifile_date'] = moment().format('YYYY/MM/DD HH:mm:ss');
+
+
+        return db.patch(TBL_TP, entity, condition)
     },
 
     patch: (entity) => {
@@ -44,6 +60,6 @@ module.exports = {
 
         entity['modifile_date'] = moment().format('YYYY/MM/DD HH:mm:ss');
 
-        return db.patch(TBL_User, entity, condition)
+        return db.patch(TBL_TP, entity, condition)
     }
 };
