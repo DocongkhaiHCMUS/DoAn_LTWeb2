35b7f83d784f9a46ce99541301c63f2eb278c7aa
Lê Ngọc hiệp <hiepc3kt@gmail.com>
2020-07-30 12:33:47 +0700

    update admin router

--- routers/admin.route.js
+++ routers/admin.route.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const cateModel = require('../models/category.model');
-const moment = require('moment');
-const categoryModel = require('../models/category.model');
-
-
+const { options } = require('./home.route');
+const tagModel = require('../models/tag.model');
+const config = require('../db/config/config.json');
+const tagPostModel = require('../models/tag-post.model');
+// router.use(function (req, res, next) {
+//     req.app.set('view options', { layout: 'admin.hbs' });
+//     next();
+// })
 router.get('/', function (req, res) {
     res.render('layouts/admin.hbs', { layout: false });
 });
 //Xu li Category
 router.get('/category', async function (req, res) {
     const categoryLv1 = await cateModel.loadCat1();
-    let i = 0;
-    // convert datetime mysql to format DD/MM/YYYY with moment
-    while (i < categoryLv1.length) {
-        categoryLv1[i].create_date = moment(categoryLv1[i].create_date, 'YYYY-MM-DD HH:mm:ss').format('DD/MM/YYYY HH:mm:ss');
-        categoryLv1[i].modifile_date = moment(categoryLv1[i].modifile_date, 'YYYY-MM-DD HH:mm:ss').format('DD/MM/YYYY HH:mm:ss');
-        i++;
-    }
     res.render('viewAdmin/viewCategory/listCategory.hbs',
         {
             layout: false,
@@ -66,24 +63,17 @@ router.post('/category/update', async function (req, res) {
     res.redirect('/admin/category');
 });
 router.post('/category/restore', async function (req, res) {
-        var entity = {
-            id: req.body.id,
-            name: req.body.name,
-            delete: 0
-        }
-        await cateModel.patch1(entity);
+    var entity = {
+        id: req.body.id,
+        name: req.body.name,
+        delete: 0
+    }
+    await cateModel.patch1(entity);
     res.redirect('/admin/category');
 });
 router.get('/category/detail/:catID', async function (req, res) {
     const id = +req.params.catID || -1;
     const categoryLv2 = await cateModel.single2(req.params.catID);
-    let i = 0;
-    while (i < categoryLv2.length) {
-        categoryLv2[i].create_date = moment(categoryLv2[i].create_date, 'YYYY-MM-DD HH:mm:ss').format('DD/MM/YYYY HH:mm:ss');
-        categoryLv2[i].modifile_date = moment(categoryLv2[i].modifile_date, 'YYYY-MM-DD HH:mm:ss').format('DD/MM/YYYY HH:mm:ss');
-        i++;
-    }
-
     res.render('viewAdmin/viewCategory/detail.hbs', {
         layout: false,
         category: categoryLv2,
@@ -98,7 +88,7 @@ router.get('/categorylv2/add/:id', async function (req, res) {
 });
 router.post('/categorylv2/add', async function (req, res) {
     await cateModel.add2(req.body);
-    res.redirect('/admin/category');
+    res.redirect(req.headers.referer);
 });
 router.get('/categorylv2/edit/:id', async function (req, res) {
     const id = +req.params.id || -1;
@@ -121,12 +111,12 @@ router.post('/categorylv2/update', async function (req, res) {
     await cateModel.patch2(req.body);
     //   const url = req.query.retUrl || '/';
     //  res.redirect(url);
-    res.redirect('/admin/category');
+    res.redirect(req.headers.referer);
 });
 router.post('/categorylv2/restore', async function (req, res) {
     cat1 = await cateModel.singleCat1ByCat2ID(req.body.id);
     if (cat1 === null) {
-       return  res.send("Category lv1 is locked");
+        return res.send("Category lv1 is locked");
     }
     else {
         var entity = {
@@ -136,7 +126,7 @@ router.post('/categorylv2/restore', async function (req, res) {
         }
         await cateModel.patch2(entity);
     }
-    res.redirect('/admin/category');
+    res.redirect(req.headers.referer);
 });
 router.post('/categorylv2/del', async function (req, res) {
     var entity = {
@@ -145,8 +135,156 @@ router.post('/categorylv2/del', async function (req, res) {
         delete: 1
     }
     await cateModel.patch2(entity);
-    res.redirect('/admin/category');
+    res.redirect(req.headers.referer);
 });
-//Xu li POST
+//                                                XU LI TAG
+router.get('/tag', async function (req, res) {
+    const page = +req.query.page || 1;
+    if (page < 0 || !page) {
+        page = 1;
+    }
+    const offset = (page - 1) * config.pagination.limit;
 
+    const [list, total] = await Promise.all([
+        //config.pagination.limit
+        tagModel.pageByTag(config.pagination.limit, offset),
+        tagModel.countByTag()
+    ]);
+
+    // const total = await productModel.countByCat(req.params.catId);
+    const nPages = Math.ceil(total / config.pagination.limit);
+    const pageItems = [];
+
+    if (1 == page)
+        pageItems.push({
+            'value': 1,
+            'active': true
+        });
+    else
+        pageItems.push({
+            'value': 1,
+        });
+
+    if (page - 3 > 2) {
+        pageItems.push({
+            'value': '...',
+            'disable': true
+        });
+    }
+
+    let top = (page + 3 < nPages - 1 ? page + 3 : nPages - 1);
+    let bot = (page - 3 > 1 ? page - 3 : 2);
+    for (let i = bot; i <= top; i++) {
+        if (i == page)
+            pageItems.push({
+                'value': i,
+                'active': true
+            });
+        else
+            pageItems.push({ 'value': i });
+    }
+
+    if (page + 3 < nPages - 1) {
+        pageItems.push({
+            'value': '...',
+            'disable': true
+        });
+    }
+
+    if (nPages == page && nPages != 1)
+        pageItems.push({
+            'value': nPages,
+            'active': true
+        });
+    else if (nPages > 1)
+        pageItems.push({
+            'value': nPages
+        });
+
+    res.render('viewAdmin/viewTag/list.hbs',
+        {
+            layout: false,
+            tags: list,
+            empty: list.length === 0,
+            pageItems,
+            prev_value: page - 1,
+            next_value: page + 1,
+            can_go_prev: page > 1,
+            can_go_next: page < nPages
+        });
+});
+router.get('/tag/add', function (req, res) {
+    res.render('viewAdmin/viewTag/add.hbs', {
+        layout: false
+    });
+});
+
+router.post('/tag/add', async function (req, res) {
+    console.log(req.body);
+    await tagModel.add(req.body);
+    res.render('viewAdmin/viewTag/add.hbs', {
+        layout: false
+    });
+});
+
+router.get('/tag/edit/:id', async function (req, res) {
+    // const id = +req.query.id || -1;
+    const id = +req.params.id || -1;
+    const rows = await tagModel.singleTagById(id);
+    if (rows.length === 0)
+        return res.send('Invalid parameter.');
+
+    const tag = rows[0];
+    res.render('viewAdmin/viewTag/edit.hbs', { layout: false, tag });
+});
+
+router.post('/tag/del', async function (req, res) {
+    var entity = {
+        id: req.body.id,
+        name: req.body.name,
+        des: req.body.des,
+        delete: 1
+    }
+    await tagModel.patch(entity);
+    await tagModel.deteleAllTagPostByTag(req.body.id);
+    res.redirect('/admin/tag');
+});
+
+router.post('/tag/update', async function (req, res) {
+    await tagModel.patch(req.body);
+    res.redirect('/admin/tag');
+});
+router.post('/tag/restore', async function (req, res) {
+    var entity = {
+        id: req.body.id,
+        name: req.body.name,
+        des: req.body.des,
+        delete: 0
+    }
+    await tagModel.patch(entity);
+    res.redirect('/admin/tag');
+});
+router.get('/tag/detail/:id', async function (req, res) {
+    const id = +req.params.id || -1;
+    const tag_post = await tagPostModel.singleByTag(id);
+    res.render('viewAdmin/viewTag/detail.hbs', {
+        layout: false,
+        tag_post,
+        empty: tag_post.length === 0
+    });
+});
+///                           DELETE , RESTORE TAG_POST
+router.post('/tag_post/restore', async function (req, res) {
+    var entity = {
+        id: req.body.id,
+        delete: 0
+    }
+    await tagPostModel.restore(req.body.id)
+    res.redirect(req.headers.referer);
+});
+router.post('/tag_post/del', async function (req, res) {
+    await tagPostModel.delete(req.body.id)
+    const url = req.query.retUrl || '/';
+    res.redirect(req.headers.referer);
+});
 module.exports = router;
