fc9fdb7e140f20ca4dbc26e8fbdbbc3acc4ebe0f
Lê Ngọc hiệp <hiepc3kt@gmail.com>
2020-08-08 13:42:55 +0700

    fix bug category model

--- models/category.model.js
+++ models/category.model.js
@@ -10,8 +10,8 @@ module.exports = {
     join ${TBL_Cat1} cat1 on cat2.category_level1 = cat1.id and cat1.delete = 0 and cat2.delete = 0`),
     load_cat1: () => db.load(`select cat1.id id,cat1.name name from ${TBL_Cat1} cat1 where cat1.delete = 0`),
     load_cat2: () => db.load(`select cat2.id id,cat2.name name,cat2.category_level1 from ${TBL_Cat2} cat2 where cat2.delete = 0`),
-    loadCat1ByAssign: (id) => db.load(`select cat1.id id,cat1.name name from ${TBL_Cat1} cat1 , ${TBL_Assign} ass
-     where cat1.delete = 0 and cat1.id <> ass.category and ass.user = ${id}`),
+    loadCat1ByAssign: (id) => db.load(`select cat1.id id,cat1.name from ${TBL_Cat1} cat1 where cat1.delete = 0
+     and cat1.id not in (select category from ${TBL_Assign} where user = ${id})`),
 
     singleCatByIDCat2: (ID) => db.load(`select cat2.id id_cat2,cat2.name name_cat2,cat1.id id_cat1, cat1.name name_cat1 from ${TBL_Cat2} cat2 join 
     ${TBL_Cat1} cat1 on cat2.category_level1 = cat1.id and cat2.id = ${ID} and cat2.delete = 0`),
