53bd1c8b934351154d6a98d78021d8990d6afe2f
Docongkhai <58779157+Docongkhai@users.noreply.github.com>
2020-08-12 07:42:40 +0700

    update new database for project

--- routers/post.route.js
+++ routers/post.route.js
@@ -7,93 +7,93 @@ const modelCategory = require('../models/category.model');
 const modelComment = require('../models/user-comment.model');
 const moment = require('moment');
 
-function handlingP(str, img_cap, pos_cap) {
-    //replace image
-    if (str.includes('+++')) {
-        str =
-            `<figure>
-            <img src="/public/img/img_post/${img_cap[pos_cap.i].folder}/${img_cap[pos_cap.i].name_img}.jpg" alt="" style="width:100%">
-            <figcaption>${img_cap[pos_cap.i].caption}</figcaption>
-        </figure>`;
-        pos_cap.i = pos_cap.i + 1;
-    }
+// function handlingP(str, img_cap, pos_cap) {
+//     //replace image
+//     if (str.includes('+++')) {
+//         str =
+//             `<figure>
+//             <img src="/public/img/img_post/${img_cap[pos_cap.i].folder}/${img_cap[pos_cap.i].name_img}.jpg" alt="" style="width:100%">
+//             <figcaption>${img_cap[pos_cap.i].caption}</figcaption>
+//         </figure>`;
+//         pos_cap.i = pos_cap.i + 1;
+//     }
 
-    if (str.includes('xxx')) {
+//     if (str.includes('xxx')) {
 
-        str =
-            `<figure>
-            <img src="/public/img/img_post/${img_cap[pos_cap.i].folder}/${img_cap[pos_cap.i].name_img}.jpg" alt="" style="width:100%">
-        </figure>`;
-        pos_cap.i = pos_cap.i + 1;
-    }
+//         str =
+//             `<figure>
+//             <img src="/public/img/img_post/${img_cap[pos_cap.i].folder}/${img_cap[pos_cap.i].name_img}.jpg" alt="" style="width:100%">
+//         </figure>`;
+//         pos_cap.i = pos_cap.i + 1;
+//     }
 
-    //replace special text
-    let pat = /<em\*\* ([^\<\>\*]+) \*\*em>/g;
-    let pat_1 = /<em\*\* ([^\<\>\*]+) \*\*em>\s*\r\n/g;
-    if (pat_1.test(str)) {
-        str = str.replace(pat_1, function (x, group1) {
-            return `<\p><p class="post_des"><em>${group1}</em></p><p class="post_des">`;
-        });
-    }
-    else if (pat.test(str)) {
-        str = str.replace(pat, function (x, group1) {
-            return `<em>${group1}</em>`;
-        });
-    }
+//     //replace special text
+//     let pat = /<em\*\* ([^\<\>\*]+) \*\*em>/g;
+//     let pat_1 = /<em\*\* ([^\<\>\*]+) \*\*em>\s*\r\n/g;
+//     if (pat_1.test(str)) {
+//         str = str.replace(pat_1, function (x, group1) {
+//             return `<\p><p class="post_des"><em>${group1}</em></p><p class="post_des">`;
+//         });
+//     }
+//     else if (pat.test(str)) {
+//         str = str.replace(pat, function (x, group1) {
+//             return `<em>${group1}</em>`;
+//         });
+//     }
 
-    let pat1 = /<strong\*\* ([^\<\>\*]+) \*\*strong>/g;
-    let pat1_1 = /<strong\*\* ([^\<\>\*]+) \*\*strong>\s*\r\n/g;
-    if (pat1_1.test(str)) {
-        str = str.replace(pat1_1, function (x, group1) {
-            return `<\p><p class="post_des"><strong>${group1}</strong></p><p class="post_des">`;
-        });
-    }
-    else if (pat1.test(str)) {
-        str = str.replace(pat1, function (x, group1) {
-            return `<strong>${group1}</strong>`;
-        });
-    }
+//     let pat1 = /<strong\*\* ([^\<\>\*]+) \*\*strong>/g;
+//     let pat1_1 = /<strong\*\* ([^\<\>\*]+) \*\*strong>\s*\r\n/g;
+//     if (pat1_1.test(str)) {
+//         str = str.replace(pat1_1, function (x, group1) {
+//             return `<\p><p class="post_des"><strong>${group1}</strong></p><p class="post_des">`;
+//         });
+//     }
+//     else if (pat1.test(str)) {
+//         str = str.replace(pat1, function (x, group1) {
+//             return `<strong>${group1}</strong>`;
+//         });
+//     }
 
 
-    let pat2 = /<under\*\* ([^\<\>\*]+) \*\*under>/g;
-    let pat2_1 = /<under\*\* ([^\<\>\*]+) \*\*under>\s*\r\n/g;
-    if (pat2_1.test(str)) {
-        str = str.replace(pat1_1, function (x, group1) {
-            return `<\p><p class="post_des"><u>${group1}</u></p><p class="post_des">`;
-        });
-    }
-    else if (pat2.test(str)) {
-        str = str.replace(pat2, function (x, group1) {
-            return `<u>${group1}</u>`;
-        });
-    }
+//     let pat2 = /<under\*\* ([^\<\>\*]+) \*\*under>/g;
+//     let pat2_1 = /<under\*\* ([^\<\>\*]+) \*\*under>\s*\r\n/g;
+//     if (pat2_1.test(str)) {
+//         str = str.replace(pat1_1, function (x, group1) {
+//             return `<\p><p class="post_des"><u>${group1}</u></p><p class="post_des">`;
+//         });
+//     }
+//     else if (pat2.test(str)) {
+//         str = str.replace(pat2, function (x, group1) {
+//             return `<u>${group1}</u>`;
+//         });
+//     }
 
-    let pat3 = /<sup\*\* ([^\<\>\*]+) \*\*sup>/g;
-    if (pat3.test(str)) {
-        str = str.replace(pat3, function (x, group1) {
-            return `<sup>${group1}</sup>`;
-        });
-    }
+//     let pat3 = /<sup\*\* ([^\<\>\*]+) \*\*sup>/g;
+//     if (pat3.test(str)) {
+//         str = str.replace(pat3, function (x, group1) {
+//             return `<sup>${group1}</sup>`;
+//         });
+//     }
 
-    //push into p
-    str = `<p class="post_des">${str}</p>`;
+//     //push into p
+//     str = `<p class="post_des">${str}</p>`;
 
-    return str;
-}
+//     return str;
+// }
 
-//handling post
-function convertContent(post, img_cap) {
-    let content = post.full_des;
-    let res = content.split("\r\n\r");
+// //handling post
+// function convertContent(post, img_cap) {
+//     let content = post.full_des;
+//     let res = content.split("\r\n\r");
 
-    var pos_cap = { 'i': 0 }
-    for (let i = 0; i < res.length; i++) {
-        res[i] = { 'text': handlingP(res[i], img_cap, pos_cap) };
-    }
+//     var pos_cap = { 'i': 0 }
+//     for (let i = 0; i < res.length; i++) {
+//         res[i] = { 'text': handlingP(res[i], img_cap, pos_cap) };
+//     }
 
-    post.full_des = res;
-    return post;
-}
+//     post.full_des = res;
+//     return post;
+// }
 
 router.get('/:id', async function (req, res) {
 
@@ -131,7 +131,7 @@ router.get('/:id', async function (req, res) {
     }
 
     // image_caption
-    post = convertContent(post, img_cap);
+    // post = convertContent(post, img_cap);
 
     // comment
     listComment.sort(function (a, b) {
